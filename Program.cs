29eac514519386117939aea57c1ea5d0e1327057
Katarina <katarina.reponen2002@gmail.com>
2022-11-25 11:36:39 +0200

    add test2 code

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
